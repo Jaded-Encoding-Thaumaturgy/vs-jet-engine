3faf218dd02291de465fe894b808d389aa4b7c7b
Ichunjo <ichunjo.le.terrible@gmail.com>
2025-12-25 02:46:53 +0100

    test: remove obsolete vapoursynth core fixtures

--- tests/fixtures/pytest_core_stored_in_test.py
+++
@@ -1,7 +0,0 @@
-from vapoursynth import core
-
-test = [0]
-
-
-def test_fails_core_stored_in_text():
-    test[0] = core.std.BlankClip()
--- tests/fixtures/pytest_core_succeeds.py
+++
@@ -1,5 +0,0 @@
-from vapoursynth import core
-
-
-def test_core_succeeds():
-    core.std.BlankClip().get_frame(0)
